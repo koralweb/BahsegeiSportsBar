d2c0e74028323066ee0ac42d1c953a41c03aea19
koralweb <dev@koralweb.com>
2023-07-05 16:39:09 +0300

    kosjak

--- src/sreens/BookingScreen.js
+++ src/sreens/BookingScreen.js
@@ -132,7 +132,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     alignSelf: 'center',
-    marginTop: 200,
+    marginTop: 'auto',
   },
   btnText: {
     fontSize: 18,
